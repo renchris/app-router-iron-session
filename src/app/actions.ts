30f1712d8a383d92506833072efaf38df8bb7894
Chris Ren <ren.chris@outlook.com>
2023-08-24 20:30:00 -0700

    feat: remove regular cookie functions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -18,33 +18,3 @@ export const readCookieFromStorageServerActionIronSession = async (): Promise<st
 export const submitCookieToStorageServerAction = async (cookie: string) => {
   cookies().set('cookieName', cookie)
 }
-
-export const readCookieToStorageServerAction = async (): Promise<string> => {
-  const cookieStore = cookies()
-  const currentCookie = cookieStore.get('cookieName')
-  return currentCookie ? currentCookie.value : 'Cookie Not Found!'
-}
-
-export const submitCookieToStorage = async (cookie: string) => {
-  await fetch('http://localhost:3000/api/submitCookie', {
-    method: 'POST',
-    body: JSON.stringify({
-      cookie,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-}
-
-export const readCookieStorage = async (): Promise<string> => {
-  const responseWithCookieFromStorage = await fetch('http://localhost:3000/api/readCookieStorage', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  const data = await responseWithCookieFromStorage.json();
-  const cookieValue = data?.cookieInStorage?.value || 'No Cookie In Storage'
-  return cookieValue
-}
