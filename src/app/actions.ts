fc7e3d209b6874113c5af9b67ae48a8d3c8332a4
Chris Ren <ren.chris@outlook.com>
2023-08-24 20:15:38 -0700

    feat: server action and API route handler functions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,6 +2,19 @@
 
 import { cookies } from "next/headers"
 
+import { getServerActionSession } from '../../lib/session'
+
+export const submitCookieToStorageServerActionIronSession = async (cookie: string) => {
+  const session = await getServerActionSession()
+  session.cookieName = cookie
+  await session.save()
+}
+
+export const readCookieFromStorageServerActionIronSession = async (): Promise<string> => {
+  const session = await getServerActionSession()
+  return session.cookieName || 'Cookie Not Found!'
+}
+
 export const submitCookieToStorageServerAction = async (cookie: string) => {
   cookies().set('cookieName', cookie)
 }
@@ -13,7 +26,7 @@ export const readCookieToStorageServerAction = async (): Promise<string> => {
 }
 
 export const submitCookieToStorage = async (cookie: string) => {
-    await fetch('http://localhost:3000/api/submitCookie', {
+  await fetch('http://localhost:3000/api/submitCookie', {
     method: 'POST',
     body: JSON.stringify({
       cookie,
@@ -25,7 +38,7 @@ export const submitCookieToStorage = async (cookie: string) => {
 }
 
 export const readCookieStorage = async (): Promise<string> => {
-    const responseWithCookieFromStorage = await fetch('http://localhost:3000/api/readCookieStorage', {
+  const responseWithCookieFromStorage = await fetch('http://localhost:3000/api/readCookieStorage', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -33,29 +46,5 @@ export const readCookieStorage = async (): Promise<string> => {
   })
   const data = await responseWithCookieFromStorage.json();
   const cookieValue = data?.cookieInStorage?.value || 'No Cookie In Storage'
-  return cookieValue
-}
-
-export const submitIronSessionCookieToStorage = async (cookie: string) => {
-    await fetch('http://localhost:3000/api/submitIronSessionCookie', {
-    method: 'POST',
-    body: JSON.stringify({
-      cookie,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-}
-
-export const readIronSessionCookieStorage = async (): Promise<string> => {
-    const responseWithCookieFromStorage = await fetch('http://localhost:3000/api/readIronSessionCookieStorage', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  const data = await responseWithCookieFromStorage.json();
-  const cookieValue = data?.cookieInStorage || 'No Cookie In Storage'
   return cookieValue
 }
---
+++ src/app/clientActions.ts
@@ -0,0 +1,36 @@
+'use client'
+
+export const submitCookieToStorageUseClient = async (cookie: string) => {
+  await fetch('http://localhost:3000/api/submitCookie', {
+    method: 'POST',
+    body: JSON.stringify({
+      cookie,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+export const submitIronSessionCookieToStorage = async (cookie: string) => {
+  await fetch('http://localhost:3000/api/submitIronSessionCookie', {
+    method: 'POST',
+    body: JSON.stringify({
+      cookie,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+export const readIronSessionCookieFromStorage = async (): Promise<string> => {
+  const responseWithCookieFromStorage = await fetch('http://localhost:3000/api/readIronSessionCookieStorage', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const data = await responseWithCookieFromStorage.json();
+  const cookieValue = data?.cookieInStorage || 'No Cookie In Storage'
+  return cookieValue
+}
